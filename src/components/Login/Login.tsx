5e4ff568cdaba0a89819ef0da8e7ae6b4f5620ef
jany_coder <jany_ete@yahoo.com>
2023-06-22 12:06:01 +0600

    sign up design complete

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -87,7 +87,25 @@ const Login = () => {
                             <label className='text-sm' htmlFor="male">Others</label>
                         </div>
                     </div>
-
+                    {/* Birth Date */}
+                    <div>
+                        <div>
+                            <div className="flex items-center justify-between">
+                                <label htmlFor="birtdate" className="block text-sm font-medium leading-6 text-gray-900">
+                                    Birthdate
+                                </label>
+                            </div>
+                            <div className="mt-2">
+                                <input
+                                    id="dob"
+                                    name="date"
+                                    type="date"
+                                    required
+                                    className="block w-full rounded-md border-0 p-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                />
+                            </div>
+                        </div>
+                    </div>
                     <div>
                         <button
                             type="submit"
