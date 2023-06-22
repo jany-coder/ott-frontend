c3fc40e1a7669eaf3b826041909dceb2b332f579
jany_coder <jany_ete@yahoo.com>
2023-06-22 12:21:28 +0600

    sign in and sign up page complete

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,11 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Login = () => {
+    const [showSignupField, setShowSignupField] = useState(false);
     return (
         <div className="flex min-h-screen flex-1 flex-col justify-center px-6 py-12 lg:px-8">
             <div className="sm:mx-auto sm:w-full sm:max-w-sm">
                 <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
-                    Sign in to your account
+                    {showSignupField ? <div>Sign Up To Your Account</div> : <div>Sign In To Your Account</div>}
                 </h2>
             </div>
 
@@ -44,68 +45,74 @@ const Login = () => {
                             />
                         </div>
                     </div>
-                    <div className='flex gap-4 items-center'>
-                        <div className="flex items-center justify-between">
-                            <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
-                                Gender
-                            </label>
-                        </div>
-                        {/* Male */}
-                        <div className="flex gap-2">
-                            <input
-                                id="male"
-                                name="gender"
-                                type="radio"
-                                autoComplete="current-password"
-                                required
-                                className="block"
-                            />
-                            <label className='text-sm' htmlFor="male">Male</label>
-                        </div>
-                        {/* Female */}
-                        <div className="flex gap-2">
-                            <input
-                                id="female"
-                                name="gender"
-                                type="radio"
-                                autoComplete="current-password"
-                                required
-                                className="block"
-                            />
-                            <label className='text-sm' htmlFor="male">Female</label>
-                        </div>
-                        {/* Others */}
-                        <div className="flex gap-2">
-                            <input
-                                id="male"
-                                name="gender"
-                                type="radio"
-                                autoComplete="current-password"
-                                required
-                                className="block"
-                            />
-                            <label className='text-sm' htmlFor="male">Others</label>
-                        </div>
-                    </div>
-                    {/* Birth Date */}
-                    <div>
-                        <div>
-                            <div className="flex items-center justify-between">
-                                <label htmlFor="birtdate" className="block text-sm font-medium leading-6 text-gray-900">
-                                    Birthdate
-                                </label>
-                            </div>
-                            <div className="mt-2">
-                                <input
-                                    id="dob"
-                                    name="date"
-                                    type="date"
-                                    required
-                                    className="block w-full rounded-md border-0 p-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                                />
-                            </div>
-                        </div>
-                    </div>
+                    {
+                        showSignupField && (
+                            <>
+                                <div className='flex gap-4 items-center'>
+                                    <div className="flex items-center justify-between">
+                                        <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
+                                            Gender
+                                        </label>
+                                    </div>
+                                    {/* Male */}
+                                    <div className="flex gap-2">
+                                        <input
+                                            id="male"
+                                            name="gender"
+                                            type="radio"
+                                            autoComplete="current-password"
+                                            required
+                                            className="block"
+                                        />
+                                        <label className='text-sm' htmlFor="male">Male</label>
+                                    </div>
+                                    {/* Female */}
+                                    <div className="flex gap-2">
+                                        <input
+                                            id="female"
+                                            name="gender"
+                                            type="radio"
+                                            autoComplete="current-password"
+                                            required
+                                            className="block"
+                                        />
+                                        <label className='text-sm' htmlFor="male">Female</label>
+                                    </div>
+                                    {/* Others */}
+                                    <div className="flex gap-2">
+                                        <input
+                                            id="male"
+                                            name="gender"
+                                            type="radio"
+                                            autoComplete="current-password"
+                                            required
+                                            className="block"
+                                        />
+                                        <label className='text-sm' htmlFor="male">Others</label>
+                                    </div>
+                                </div>
+                                {/* Birth Date */}
+                                <div>
+                                    <div>
+                                        <div className="flex items-center justify-between">
+                                            <label htmlFor="birtdate" className="block text-sm font-medium leading-6 text-gray-900">
+                                                Birthdate
+                                            </label>
+                                        </div>
+                                        <div className="mt-2">
+                                            <input
+                                                id="dob"
+                                                name="date"
+                                                type="date"
+                                                required
+                                                className="block w-full rounded-md border-0 p-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                            />
+                                        </div>
+                                    </div>
+                                </div>
+                            </>
+                        )
+                    }
                     <div>
                         <button
                             type="submit"
@@ -117,8 +124,8 @@ const Login = () => {
                 </form>
                 <p className="mt-10 text-center text-sm text-gray-500">
                     Not a member?{' '}
-                    <button type='button' className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">
-                        Sign Up
+                    <button onClick={() => setShowSignupField(!showSignupField)} type='button' className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">
+                        {showSignupField ? <div>Sign In</div> : <div>Sign Up</div>}
                     </button>
                 </p>
             </div>
